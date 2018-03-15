9c8b6a684debf09b79013951cccedfc13f795491
Ayush Gupta <7ayushgupta@gmail.com>
2018-03-15 18:36:03 +0530

    Sending data to frontend

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var express  = require('express');
 var app = express();
 var server = require('http').Server(app);
 var socketio = require('socket.io')(server);
-
 var shell = require('shelljs')
+var fs = require('fs');
+const summaryFolder = './summary';
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
@@ -16,7 +17,20 @@ socketio.on('connection', function(socket){
 		console.log('Trying to fetch data');
 		shell.exec('bash ./setup.sh');
 		console.log("Bash script ran completely");
+		console.log("Trying to read data");
+		fs.readdirSync(summaryFolder).forEach(file => {
+			fs.readFile(summaryFolder + '/' + file, "utf8", function(err, data) {
+				console.log(data);
+				sendData(data);
+			});
+			console.log(file);
+		})
 	});
+
+	function sendData(file){
+		socketio.emit("senddata", file);
+		console.log("data sent");
+	}
 });
 
 var port = process.env.PORT || 1337;
